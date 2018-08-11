30c05526a0c821bc9a1f96994df3e666f52c0357
qingwen.ye <yhl@1024hw.cn>
2018-08-11 23:46:45 +0800

    add env

--- src/index.js
+++ src/index.js
@@ -5,20 +5,21 @@ const updateUser = require('./jobs/user')
 const updateUserMeter = require('./jobs/user_meter')
 
 updateMeter()
+console.log(config)
 setInterval(() => {
   console.log('updateMeter starting ................ ')
   updateMeter()
   console.log('updateMeter ending ................ ')
-}, 30 * 60 * 1000)
+}, config.interval.meter * 60 * 1000)
 
 setInterval(() => {
   console.log('updateUser starting ................ ')
   updateUser()
   console.log('updateUser ending ................ ')
-}, 60 * 60 * 1000)
+}, config.interval.user * 60 * 1000)
 
 setInterval(() => {
   console.log('updateUserMeter starting ................ ')
   updateUserMeter()
   console.log('updateUserMeter ending ................ ')
-}, 60 * 60 * 1000)
+}, config.interval.userMeter * 60 * 1000)
