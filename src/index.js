1dfc4dba14878ba99c5f879feb4e41809ed39b11
qingwen.ye <yhl@1024hw.cn>
2018-08-11 20:51:39 +0800

    update

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ const updateMeter = require('./jobs/meter')
 const updateUser = require('./jobs/user')
 const updateUserMeter = require('./jobs/user_meter')
 
-updateMeter()
 setInterval(() => {
   console.log('updateMeter starting ................ ')
   updateMeter()
