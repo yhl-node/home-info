edfd91cd60b62db479c451a20f06eee2d2747f82
yhl <yhl@1024hw.cn>
2018-08-11 20:52:11 +0800

    add auto start (#5)
    * add auto start
    
    * update

--- src/index.js
+++ src/index.js
@@ -4,13 +4,19 @@ const updateUser = require('./jobs/user')
 const updateUserMeter = require('./jobs/user_meter')
 
 setInterval(() => {
+  console.log('updateMeter starting ................ ')
   updateMeter()
+  console.log('updateMeter ending ................ ')
 }, 30 * 60 * 1000)
 
 setInterval(() => {
+  console.log('updateUser starting ................ ')
   updateUser()
+  console.log('updateUser ending ................ ')
 }, 60 * 60 * 1000)
 
 setInterval(() => {
+  console.log('updateUserMeter starting ................ ')
   updateUserMeter()
+  console.log('updateUserMeter ending ................ ')
 }, 60 * 60 * 1000)
