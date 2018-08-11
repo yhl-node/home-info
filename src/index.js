758135a9b88101122a699781527d0d28998925f2
qingwen.ye <yhl@1024hw.cn>
2018-08-11 20:49:36 +0800

    add auto start

--- src/index.js
+++ src/index.js
@@ -3,14 +3,21 @@ const updateMeter = require('./jobs/meter')
 const updateUser = require('./jobs/user')
 const updateUserMeter = require('./jobs/user_meter')
 
+updateMeter()
 setInterval(() => {
+  console.log('updateMeter starting ................ ')
   updateMeter()
+  console.log('updateMeter ending ................ ')
 }, 30 * 60 * 1000)
 
 setInterval(() => {
+  console.log('updateUser starting ................ ')
   updateUser()
+  console.log('updateUser ending ................ ')
 }, 60 * 60 * 1000)
 
 setInterval(() => {
+  console.log('updateUserMeter starting ................ ')
   updateUserMeter()
+  console.log('updateUserMeter ending ................ ')
 }, 60 * 60 * 1000)
