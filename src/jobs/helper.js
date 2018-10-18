f2f46c756ba8255bf422e8e50eb0d0ca8f78f98a
qingwen.ye <yhl@1024hw.cn>
2018-10-18 20:36:38 +0800

    fix

--- src/jobs/helper.js
+++ src/jobs/helper.js
@@ -2,12 +2,21 @@
  * @Author: yhl, yhl@1024hw.org
  * @Date: 2018-08-11 23:10:57
  * @Last Modified by: yhl
- * @Last Modified time: 2018-08-18 16:28:32
+ * @Last Modified time: 2018-10-18 20:34:16
  */
 import Sequelize from 'sequelize'
 import config from 'config'
 
-const sequelize = new Sequelize(config.mysql.url, {
+// const sequelize = new Sequelize(config.mysql.url, {
+//   define: { timestamps: false },
+//   logging: false
+// })
+const sequelize = new Sequelize({
+  host: config.mysql.host,
+  username: config.mysql.user,
+  password: config.mysql.pwd,
+  database: config.mysql.db,
+  dialect: 'mysql',
   define: { timestamps: false },
   logging: false
 })
